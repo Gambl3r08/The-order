fb2b8377093d858eaae45d1d8680124170f6a14c
Roberto Lozada <robertolozada_@outlook.com>
2024-09-10 00:57:38 -0500

    add cors

--- app/main.py
+++ app/main.py
@@ -4,12 +4,21 @@ from app.services.ControllerLogService import router as log_router
 from app.services.OrderService import router as order_router
 from app.core.db import engine, Base
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(
     product_router, tags=["Products"], prefix="/api/v1")
