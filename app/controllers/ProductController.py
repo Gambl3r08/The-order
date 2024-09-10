8565ed5098cb240af6036b4ccdecb68f75e68a06
Roberto Lozada <robertolozada_@outlook.com>
2024-09-09 21:39:54 -0500

    product crud

--- app/controllers/ProductController.py
+++ app/controllers/ProductController.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from app.models.Product import Product
 from app.schemas.ProductSchemma import ProductResponse
 from app.schemas.ProductSchemma import ProductCreate
+import uuid
 
 
 class ProductController:
@@ -14,3 +15,35 @@ class ProductController:
         self.db.commit()
         self.db.refresh(db_product)
         return ProductResponse.model_validate(db_product)
+
+    def get_products(self):
+        products = self.db.query(Product).filter(Product.active == 1).all()
+        return products
+
+    def get_product_by_id(self, product_id: uuid.UUID):
+        product = self.db.query(
+            Product).filter(Product.product_id == product_id).first()
+        return product
+
+    def update_product(self, product_id: uuid.UUID, product: ProductCreate):
+        db_product = self.db.query(
+            Product).filter(Product.product_id == product_id).first()
+        if not db_product:
+            return None
+        db_product.product_name = product.product_name
+        db_product.price = product.price
+        db_product.description = product.description
+        db_product.stock = product.stock
+        db_product.quantity = product.quantity
+        self.db.commit()
+        self.db.refresh(db_product)
+        return ProductResponse.model_validate(db_product)
+
+    def delete_product(self, product_id: uuid.UUID):
+        db_product = self.db.query(
+            Product).filter(Product.product_id == product_id).first()
+        if not db_product:
+            return None
+        self.db.delete(db_product)
+        self.db.commit()
+        return db_product
--- app/services/ProductService.py
+++ app/services/ProductService.py
@@ -30,3 +30,69 @@ async def create_product(product: ProductCreate):
         return JSONResponse(content=json_response)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/products")
+async def get_products():
+    try:
+        product_controller = ProductController(SessionLocal())
+        data = product_controller.get_products()
+        if data is None:
+            response = {
+                "status": StatusCodes.STATUS_CODE_NOT_FOUND,
+                "message": "Products not found",
+                "data": data
+            }
+        response = {
+            "status": StatusCodes.STATUS_CODE_OK,
+            "message": "Products found successfully",
+            "data": data
+        }
+        json_response = jsonable_encoder(response)
+        return JSONResponse(content=json_response)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.put("/products/{product_id}")
+async def update_product(product_id: str, product: ProductCreate):
+    try:
+        product_controller = ProductController(SessionLocal())
+        data = product_controller.update_product(product_id, product)
+        if data is None:
+            response = {
+                "status": StatusCodes.STATUS_CODE_NOT_FOUND,
+                "message": "Product not found",
+                "data": data
+            }
+        response = {
+            "status": StatusCodes.STATUS_CODE_OK,
+            "message": "Product updated successfully",
+            "data": data
+        }
+        json_response = jsonable_encoder(response)
+        return JSONResponse(content=json_response)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.delete("/products/{product_id}")
+async def delete_product(product_id: str):
+    try:
+        product_controller = ProductController(SessionLocal())
+        data = product_controller.delete_product(product_id)
+        if data is None:
+            response = {
+                "status": StatusCodes.STATUS_CODE_NOT_FOUND,
+                "message": "Product not found",
+                "data": data
+            }
+        response = {
+            "status": StatusCodes.STATUS_CODE_OK,
+            "message": "Product deleted successfully",
+            "data": data
+        }
+        json_response = jsonable_encoder(response)
+        return JSONResponse(content=json_response)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
